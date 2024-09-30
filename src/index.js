96d38df5f57be1320a8b157157707b08262a0eb6
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-09-30 14:00:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,33 +9,3 @@ dotenv.config({
 })
 
 connectDB()
-
-
-
-/*
-// 1st approach 
-
-import express from 'express'
-const app = express()
-
-//ifie
-(async()=> {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on('error', ()=> {
-            console.log("Error :" ,error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-            
-        })
-
-    } 
-
-    catch (error) {
-        console.error("ERROR : ", error)
-    }
-})()
-*/
