1b2f752b1f1d74eb4c36e1fa31866e0342868584
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-10-02 15:35:30 +0530

    logg out route and middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,22 @@ import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import { ApiResponse } from "../utils/apiResponse.js";
 
 
+const generateAccessAndRefreshToken = async(userId)=> {
+    try {   
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, "something went wrong while generating access and refresh token")
+    }
+}
+
 const registerUser = asyncHandler(async(req, res)=> {
     //get user details from frontend
     //validation - not empty
@@ -73,4 +89,85 @@ const registerUser = asyncHandler(async(req, res)=> {
     return res.status(201).json(new ApiResponse(200, createdUser, "User registered successfully"))
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async(req, res)=> {
+    //todos
+    //get user details
+    //username or email
+    //find the user
+    //password check
+    //do validation using token , access and refresh token
+    //chek if user already exist
+    //send cookie
+    //return response else error
+
+    const {email, username, password} = req.body
+
+    if(!username || !email) {
+        throw new ApiError(400, "username or email is required")
+    } 
+
+    const user = await User.findOne({
+        $or : [{username}, {email}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Password Incorrect")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly : true,
+        secure : true,
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user : loggedInUser, accessToken, refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+
+})
+
+
+const logoutUser = asyncHandler(async(req, res)=>{
+    await User.findByIdAndUpdate(req.user._id, {
+        $set : {
+            refreshToken : undefined
+        }
+    },
+    {
+        new : true
+    }
+    )
+
+    const options = {
+        httpOnly : true,
+        secure : true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(200, {}, "User logged out")
+
+})
+
+export {registerUser,loginUser, logoutUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,24 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 const router = Router()
-import {upload} from '../middlewares/multer.middleware.js'
+import { upload } from '../middlewares/multer.middleware.js'
+import { verifJWT } from "../middlewares/auth.middleware.js";
 
 router.route('/register').post(
     upload.fields([
         {
-            name : "avatar",
-            maxCount : 1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name : "coverImage",
-            maxCount : 1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
-    registerUser)
+registerUser)
+
+router.route('/login').post(loginUser)
+
+router.route('/logout').post(verifJWT, logoutUser)
 
 export default router
